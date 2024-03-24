ccd0c9af359066b11273d9c98c7d0cbad26618a7
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-03-24 20:16:18 +0300

    Menu updates

--- src/components/reusables/Menus.jsx
+++ src/components/reusables/Menus.jsx
@@ -3,39 +3,31 @@ import React from 'react'
 function Menus() {
     return (
         <>
-            <div className="drawer">
-                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col">
-                    {/* Navbar */} 
-                    <div className="w-full navbar bg-emerald-800">
-                        <div className="flex-none lg:hidden">
-                            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                            </label>
-                        </div>
-                        <div className="flex-1 px-2 mx-2 text-white text-2xl">🫴Msaada</div>
-                        <div className="flex-none hidden lg:block">
-                            <ul className="menu menu-horizontal">
-                                {/* Navbar menu content here */}
-                                <li className='text-lg'><a>Home</a></li>
-                                <li className='text-lg'><a>Campaigns</a></li>
-                                <li className='text-lg'><a>About us</a></li>
-                                <li className='text-lg'><a>Contact</a></li>
-                            </ul>
-                        </div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    {/* Page content here */}
-                    Content
-                </div>
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-emerald-900">
-                        {/* Sidebar content here */}
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         <li><a>Home</a></li>
                         <li><a>Campaigns</a></li>
                         <li><a>About us</a></li>
                         <li><a>Contact</a></li>
                     </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">🫴Msaada</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Home</a></li>
+                        <li><a>Campaigns</a></li>
+                        <li><a>About us</a></li>
+                        <li><a>Contact</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Log in</a>
                 </div>
             </div>
         </>
