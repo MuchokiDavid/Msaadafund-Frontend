e2737ea38335384b5d5f081f8a7ef0de2bcfa717
Clement Macharia <clementmacharia62@gmail.com>
2024-04-03 16:32:12 +0300

    updates of routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import './App.css';
-import Menus from './components/reusables/Menus';
-import Footer from './components/reusables/Footer';
 import Campaigns from './pages/Campaigns';
+import { Route, Routes } from 'react-router-dom';
+import CampainDetails from './pages/CampainDetails';
+import Home from './pages/Home';
+import Login from './components/user-auth/Login';
+import Register from './components/user-auth/Register';
+import Reset from './components/user-auth/Reset';
+import OrgLogIn from './components/org-auth/OrgLogIn';
+// import Message from './components/services/Message';
+import ContactUs from './pages/ContactUs';
+import OrgSignUp from './components/org-auth/OrgSignUp';
+import UserLayout from './components/user-dashboard/UserLayout';
+import OrgLayout from './components/org-dashboard/OrgLayout';
+import ResetPass from './components/org-auth/ResetPass';
+import AboutUs from './pages/AboutUs';
+
+
+
 function App() {
   return (
-    <>
-    <Menus/>
-    <Campaigns/>
-    <Footer/>
-    </>
+ 
+    <Routes>
+    <Route path = '/' element= {<Home/>}/>
+    <Route path='/campaign' element={<Campaigns/>}/>
+    <Route path={`campaign/:campaignId`} element={<CampainDetails/>} /> 
+    <Route path = '/user/login' element = {<Login/>}/>
+    <Route path = '/user/signup' element = {<Register/>}/>
+    <Route path = '/user/reset' element = {<Reset/>}/>
+    <Route path = '/org/reset' element = {<ResetPass/>}/>
+    <Route path = '/org/login' element = {<OrgLogIn/>}/>
+    <Route path = '/org/signup' element = {<OrgSignUp/>}/>
+    <Route path = '/contactus' element = {<ContactUs/>}/>
+    <Route path = '/about' element = {<AboutUs/>}/>
+    <Route path = '/user/dashboard/*' element = {<UserLayout/>}/>
+    <Route path = '/company/dashboard/*' element = {<OrgLayout/>}/>
+    
+    </Routes>
+   
+
+// <Route path = '/' element = {<Login />} />
+// <Route path='/signup' element={<Register/>}></Route>
+// <Route path='/resetpassword' element={<ResetPassword/>}></Route>
+// <Route path='/dashboard/*' element={<DashboardLayout/>}></Route>
+// <Route path='/verify' element={<EmailForm/>}></Route>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/usersContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
+    
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
