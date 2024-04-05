d2ddea6b3127a7ddeb3865c820de13c76832d5cb
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-05 16:30:59 +0300

    Org dashboard component and menu component added initialised

--- src/components/org-dashboard/OrgLayout.jsx
+++ src/components/org-dashboard/OrgLayout.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Menubar from './Menubar';
+import Profile from './pages/OrgProfile';
+import Transaction from './pages/Transactions';
+import { Routes, Route } from 'react-router-dom';
+import OrgHome from './pages/OrgHome';
+import DashboardNav from './DashboardNav';
 
 function OrgLayout() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1024);
+
+  // Toggle sidebar
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 1024);
+      if (!window.innerWidth >= 1024) {
+        setIsSidebarOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div>OrgLayout</div>
-  )
+    <div>
+      <DashboardNav toggleSidebar={toggleSidebar} />
+      <div className="flex">
+        <Menubar isOpen={isSidebarOpen} />
+        {/* {isLargeScreen && <Menubar isOpen={isSidebarOpen} />} */}
+        <main className="mt-3 mx-auto md:w-3/4 overflow-y-auto md:m-3 min-h-max h-1/6">
+          <Routes>
+            <Route path="/" element={<OrgHome />} />
+            <Route path="/transaction" element={<Transaction />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default OrgLayout
+export default OrgLayout;
