3f90f51c9842c2dbed99464ffcaf95980c51cc61
Michael Kiptoo <michaelkiptoo2000@gmail.com>
2024-04-07 20:20:15 +0300

    Added route to login for user

--- src/components/reusables/Menus.jsx
+++ src/components/reusables/Menus.jsx
@@ -28,7 +28,7 @@ function Menus() {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in</a>
+                    <a href="/user/login" className="btn text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in</a>
                 </div>
             </div>
         </>
