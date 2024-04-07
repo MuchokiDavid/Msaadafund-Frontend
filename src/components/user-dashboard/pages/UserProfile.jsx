9c5dc9996e3bf7763808953511b8adef9b3b0a77
josephmangara <josephmangara2@gmail.com>
2024-04-07 11:16:51 +0300

    user dashboard

--- src/components/user-dashboard/pages/UserProfile.jsx
+++ src/components/user-dashboard/pages/UserProfile.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function UserProfile() {
+  const [user, setUser] = useState({})
+
+  //  useEffect (() => {
+  //   fetch(`/api/v1.0/usersdata`, 
+  //     {method: 'GET',
+  //     headers: {
+  //       "Content-Type": "application/json"}
+  //   }
+  // );
+  // const data = await.response.json();
+  // setUser(data);
+  // console.log(data)
+  //  }
+  // )
+  
   return (
-    <div>UserProfile</div>
+    <>
+      <h1>UserProfile (username)</h1>
+      <h2>Profile picture</h2>
+      <h2>Account Settings</h2>
+      <h4>Edit username</h4>
+      <h4>change password</h4>
+      <h4>delete account</h4>
+    </>
   )
 }
 
