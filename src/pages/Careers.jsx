88338989579b6d9b5161877dd241541e00300326
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-08 12:32:21 +0300

    Career page

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Menus from '../components/reusables/Menus'
+import Footer from '../components/reusables/Footer'
 
 function Careers() {
   return (
-    <div>Careers</div>
+    <div>
+      <Menus/>
+      <div className='container'>
+        <h1 className="text-center text-3xl font-bold mt-8">Careers</h1>
+        <p className="mt-4 max-w-screen-md mx-auto text-gray-600 leading-loose text-justify" >No opening. Keep watch</p>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
