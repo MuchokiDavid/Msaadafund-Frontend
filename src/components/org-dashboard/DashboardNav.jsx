a30551c88f361ac0eee36b4e0def9bbb272b2246
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-08 22:12:49 +0300

    token added to check log in state

--- src/components/org-dashboard/DashboardNav.jsx
+++ src/components/org-dashboard/DashboardNav.jsx
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from 'react'
 import { useAuth } from '../../context/usersContext';
 import { MdOutlineAccountCircle } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
 
 
 function DashboardNav({toggleSidebar}) {
     // const [isOpen, setIsOpen] = useState(true); // Default to open on large screens
-  const {isLoggedIn, user} = useAuth();
-  console.log(user)
+  const {user} = useAuth();
+  let token=localStorage.getItem("token");
+  const navigate= useNavigate()
   
+  if(!token){
+      navigate("org/login")
+  }
+
   return (
     <div>
         <nav className="bg-gray-800 text-white flex justify-between items-center p-4">
@@ -18,7 +24,7 @@ function DashboardNav({toggleSidebar}) {
             </button>
             <h1 className="text-xl">Dashboard</h1>
             <div>
-            <h2><MdOutlineAccountCircle />Username</h2>
+            <h2><MdOutlineAccountCircle className='h-6 w-6'/>{user && user.orgName}</h2>
             </div> 
         </nav>
     </div>
