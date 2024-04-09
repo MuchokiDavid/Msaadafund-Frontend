1bd9758cb7f30f3a55a891f1433940f0c4bff43c
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-09 18:46:42 +0300

    Added styling to campaigns page

--- src/pages/Campaigns.jsx
+++ src/pages/Campaigns.jsx
@@ -106,7 +106,7 @@ const Campaigns = () => {
               <img className="w-full rounded-t-lg h-52" src={campaign.banner} alt={campaign.campaignName} />
               <div className="px-6 py-4 flex-grow">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{campaign.campaignName}</h5>
-                <p className="mb-0 text-lg text-basemb-3 font-normal text-gray-700 dark:text-gray-400 ">{campaign.category}</p>
+                <p className="mb-0 text-lg font-semibold text-basemb-3 text-gray-700 dark:text-gray-400 ">{campaign.category}</p>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-lg">{campaign.description.slice(0,80)}...</p>
                 
                 {status === 'Ongoing campaigns' && <button onClick={()=> handleCampaign(campaign.id)} class="inline-flex items-center mt-0 mb-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -127,8 +127,8 @@ const Campaigns = () => {
                   
                     {
                       status === 'Ongoing campaigns' && <div className=' px-2'>
-                        <h6 className='text-md'>Days left</h6>
                         <p className="text-black text-base dark:text-white">{calculateDaysLeft(campaign.endDate) }</p>
+                        <h6 className='text-md'>Days left</h6>
                       </div>
                     }
                     {
@@ -139,8 +139,8 @@ const Campaigns = () => {
                     }
                   
                   <div className='px-2'>
-                    <h6 className='text-md'>Budget(Ksh)</h6>
-                     <p className="text-black text-base dark:text-white">{campaign.targetAmount}</p>
+                    <h6 className='text-md ml-2'>Budget(Ksh)</h6>
+                     <p className="text-black ml-2 text-base dark:text-white">{campaign.targetAmount}</p>
                   </div>
                 </div>
 
