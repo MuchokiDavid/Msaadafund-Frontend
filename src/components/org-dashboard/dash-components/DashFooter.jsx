07f3191c83ccd6ceefcad0998afb81744ee45c82
Clement Macharia <clementmacharia62@gmail.com>
2024-04-12 01:27:01 +0300

    changes

--- src/components/org-dashboard/dash-components/DashFooter.jsx
+++ src/components/org-dashboard/dash-components/DashFooter.jsx
@@ -12,13 +12,13 @@ function DashFooter() {
     }, [])
   return (
     <>
-        <footer className="bg-emerald-800 text-gray-400 py-3">
+        <footer className="bg-emerald-800 text-white py-3">
             <div className="container ml-4">
                 <div className="row">
                 <div className="col-md-4 mb-4">
                     <h5 className='text-xl mb-1'>About Us</h5>
                     <p>Empowering you to receive donation smarter, faster, and more securely.</p>
-                    <p>Reach out to us through <a href='mailto:msaadacontact@gmail.com' className='text-md text-blue-700'>Msaada Team</a> for any questions or feedback.</p>
+                    <p>Reach out to us through <a href='mailto:msaadacontact@gmail.com' className='text-md text-blue-300'>Msaada Team</a> for any questions or feedback.</p>
                 </div>
                 </div>
                 <div className="text-center mt-2">
