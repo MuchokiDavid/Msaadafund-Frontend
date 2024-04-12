829227c62d51449e2254315ff14e94cf62ad147d
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-12 22:50:54 +0300

    Added navigation

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Featured from './Featured'
+import { useNavigate } from 'react-router-dom'
 
 function Banner() {
+  const navigate = useNavigate()
+
   return (
     <div>
       <div className="w-screen md:h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
         <img
           src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/1900/850"
-          className="absolute top-0 left-0 min-h-full ob w-full z-0"
+          className="absolute top-0 left-0 min-h-full ob w-full z-5"
           alt=""
         />
         <div className="relative max-w-screen-lg mx-auto grid grid-cols-12 h-full items-center ml-4">
-          <div className="col-span-6">
+          <div className="col-span-6 z-10">
             <span className="uppercase text-white text-xs font-bold sm:my-4 md:my:1 block">WE ARE HERE TO HELP</span>
             <h1 className="text-white font-extrabold md:text-5xl sm:text-4xl mb-8">Welcome to Our Community of Changemakers!</h1>
             <p className="text-stone-100 text-base">
@@ -28,7 +31,7 @@ function Banner() {
             <h2 className="font-black text-white text-3xl mb-4">Be Part of Something Bigger!</h2>
             <p className="text-base text-white">Join a community dedicated to driving social change and improving lives. Whether you're passionate about education, healthcare, environmental conservation, or humanitarian aid, there's a place for you here. Start your impact journey today.</p>
           </div>
-          <button className="text-white uppercase py-3 text-base px-10 border border-gray-300 hover:bg-sky-500 hover:bg-opacity-10">Get started</button>
+          <button className="text-white uppercase py-3 text-base px-10 border border-gray-300 hover:bg-sky-500 hover:bg-opacity-10">How it works</button>
         </div>
       </div>
       <Featured/>
@@ -37,12 +40,12 @@ function Banner() {
           <div className="w-full flex flex-col items-end pr-16">
             <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance</h2>
             <div className="h-full mt-auto overflow-hidden relative">
-              <img src="https://picsum.photos/800/600" className="h-full w-full object-contain" alt=""/>
+              <img src="https://images.unsplash.com/photo-1548102245-c79dbcfa9f92?q=80&w=1392&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/800/600" className="h-full w-full object-contain" alt=""/>
             </div>
           </div>
           <div className="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-sky-950 before:top-0 before:left-0">
             <div className="relative z-20 pl-12">
-              <h2 className="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Make Your Mark on the World!</h2>
+              <h2 className="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Msaada Mashinani: Empowering Change, One Donation at a Time</h2>
               <p className="text-white text-sm">
               Take the first step towards making a difference. Our platform offers you the opportunity to support various causes and organizations working tirelessly to bring about positive change. Start your journey with us today.
               </p>
@@ -61,15 +64,15 @@ function Banner() {
               <p className="text-white text-lg">
               Become part of our community and make a positive impact. Sign up now to start receiving donations and support for your cause.
               </p>
-              <button className=" mt-8 text-white uppercase py-3 text-base px-10 border border-gray-300 hover:bg-sky-500 hover:bg-opacity-10"> 
+              <button onClick={()=> navigate('/org/signup')} className=" mt-8 text-white uppercase py-3 text-base px-10 border border-gray-300 hover:bg-sky-500 hover:bg-opacity-10"> 
               Get Involved
               </button>
             </div>
           </div>
           <div className="w-full flex flex-col pl-16">
-            <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Whether you need Assistance</h2>
+            <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-left mb-12 mt-10">Spread love to the community around you</h2>
             <div className="h-full mt-auto overflow-hidden relative">
-              <img src="https://picsum.photos/800/600" className="h-full w-full object-contain" alt=""/>
+              <img src="https://img.freepik.com/free-vector/computer-online-charity-donation_24877-54452.jpg?w=740&t=st=1712950950~exp=1712951550~hmac=ba081d24a1f69dd7a1f062eb668522865498adce912871ae865e4200f98620d9/800/600" className="h-full w-full object-contain" alt=""/>
             </div>
           </div>
 
@@ -77,21 +80,21 @@ function Banner() {
       </div>
 
       <div className="py-12 relative overflow-hidden bg-white">
-        <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
-          <div className="w-full flex flex-col items-end pr-16">
+        <div className="">
+          {/* <div className="w-full flex flex-col items-end pr-16">
             <h2 className="text-[#64618C] font-bold text-2xl max-w-xs text-right mb-12 mt-10">Whether you need Assistance</h2>
             <div className="h-full mt-auto overflow-hidden relative">
               <img src="https://picsum.photos/800/600" className="h-full w-full object-contain" alt=""/>
             </div>
-          </div>
+          </div> */}
 
           <div className="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-sky-950 before:top-0 before:left-0">
             <div className="relative z-20 pl-12">
               <h2 className="text-[#f7d0b6] font-black text-5xl leading-snug mb-10">Msaada mashinani is here <br/>to help you</h2>
-              <p className="text-white text-sm">
-                Purus in massa tempor nec. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Faucibus ornare suspendisse sed nisi lacus sed viverra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar.
+              <p className="text-white text-md">
+              Join us in our mission to create a better world for all. Msaada Mashinani is your platform to make a real difference in the lives of those in need.<br/> With our easy-to-use interface and secure payment options, donating has never been more accessible. Start your journey with us today and be a part of something truly impactful.
               </p>
-              <button className="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Talk with expert</button>
+              <button onClick={()=> navigate('/campaign')} className="mt-8 text-white uppercase py-3 text-sm px-10 border border-white hover:bg-white hover:bg-opacity-10">Donate now</button>
             </div>
           </div>
         </div>
