176a6d73307bba11767ae989cc1d8b4c6bcb1916
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-12 17:59:57 +0300

    Homepage designed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Menus from '../components/reusables/Menus'
 import Footer from '../components/reusables/Footer'
+import Banner from '../components/home/Banner'
+import DonorGetStarted from '../components/home/DonorGetStarted'
 
 function Home() {
   return (
-    <div>
+    <>
       <Menus/>
-      Home
+      <div className="mx-auto overflow-x-hidden mt-3 h-auto">
+        <Banner/>
+        {/* <DonorGetStarted/> */}
+      </div>      
       <Footer/>
-    </div>
+    </>
   )
 }
 
