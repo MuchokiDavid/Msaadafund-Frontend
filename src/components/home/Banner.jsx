23c29f7124851927d89b86b63d4e8ed89cdcbad3
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-12 19:53:25 +0300

    added styling

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -4,26 +4,31 @@ import Featured from './Featured'
 function Banner() {
   return (
     <div>
-      <div className="w-screen h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
-        <img src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/1900/850" className="absolute top-0 left-0 min-h-full ob w-full" alt=""/>
-        <div className="relative z-20 max-w-screen-lg mx-auto grid grid-cols-12 h-full items-center">
+      <div className="w-screen md:h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
+        <img
+          src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/1900/850"
+          className="absolute top-0 left-0 min-h-full ob w-full z-0"
+          alt=""
+        />
+        <div className="relative max-w-screen-lg mx-auto grid grid-cols-12 h-full items-center ml-4">
           <div className="col-span-6">
-            <span className="uppercase text-white text-xs font-bold mb-2 block">WE ARE HERE TO HELP</span>
-            <h1 className="text-white font-extrabold text-5xl mb-8">Welcome to Our Community of Changemakers!</h1>
+            <span className="uppercase text-white text-xs font-bold sm:my-4 md:my:1 block">WE ARE HERE TO HELP</span>
+            <h1 className="text-white font-extrabold md:text-5xl sm:text-4xl mb-8">Welcome to Our Community of Changemakers!</h1>
             <p className="text-stone-100 text-base">
-            Join us in our mission to create a better world for all. Together, we can make a positive impact on the lives of those in need. Explore our platform and discover how you can contribute to meaningful causes today.
+              Join us in our mission to create a better world for all. Together, we can make a positive impact on the lives of
+              those in need. Explore our platform and discover how you can contribute to meaningful causes today.
             </p>
-            <a href='/campaign'><button className="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10">Get started</button></a>
+            <a href='/campaign'><button className="sm:my-6 md:mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10">Get started</button></a>
           </div>
         </div>
-      </div>
-      <div className="bg-[#f7d0b6] py-20">
+        </div>
+      <div className="bg-sky-950 py-20">
         <div className="max-w-screen-lg mx-auto flex justify-between items-center">
           <div className="max-w-xl">
-            <h2 className="font-black text-sky-950 text-3xl mb-4">Be Part of Something Bigger!</h2>
-            <p className="text-base text-sky-950">Join a community dedicated to driving social change and improving lives. Whether you're passionate about education, healthcare, environmental conservation, or humanitarian aid, there's a place for you here. Start your impact journey today.</p>
+            <h2 className="font-black text-white text-3xl mb-4">Be Part of Something Bigger!</h2>
+            <p className="text-base text-white">Join a community dedicated to driving social change and improving lives. Whether you're passionate about education, healthcare, environmental conservation, or humanitarian aid, there's a place for you here. Start your impact journey today.</p>
           </div>
-          <button className="text-sky-950 uppercase py-3 text-base px-10 border border-sky-950 hover:bg-sky-950 hover:bg-opacity-10">Get started</button>
+          <button className="text-white uppercase py-3 text-base px-10 border border-gray-300 hover:bg-sky-500 hover:bg-opacity-10">Get started</button>
         </div>
       </div>
       <Featured/>
@@ -50,13 +55,15 @@ function Banner() {
         <div className="grid grid-cols-2 max-w-screen-lg mx-auto">
           
 
-          <div className="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-[#f7d0b6] before:top-0 before:right-0">
+          <div className="py-20 bg-slate-100 relative before:absolute before:h-full before:w-screen before:bg-emerald-700 before:top-0 before:right-0">
             <div className="relative z-20 pl-12">
-              <h2 className="text-sky-950 font-black text-5xl leading-snug mb-10">Join Us and Make a Difference</h2>
-              <p className="text-sky-950 text-lg">
+              <h2 className="text-white font-black text-5xl leading-snug mb-10">Join Us and Make a Difference</h2>
+              <p className="text-white text-lg">
               Become part of our community and make a positive impact. Sign up now to start receiving donations and support for your cause.
               </p>
-              <button className="mt-8 text-sky-950 uppercase py-3 text-sm px-10 border border-sky-950 hover:bg-white hover:bg-opacity-10"> Get Involved</button>
+              <button className=" mt-8 text-white uppercase py-3 text-base px-10 border border-gray-300 hover:bg-sky-500 hover:bg-opacity-10"> 
+              Get Involved
+              </button>
             </div>
           </div>
           <div className="w-full flex flex-col pl-16">
