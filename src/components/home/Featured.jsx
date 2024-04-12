14e93ffe4cd62c30cb488c99172480b28b0f4fc1
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-12 20:27:37 +0300

    added margin to buttons

--- src/components/home/Featured.jsx
+++ src/components/home/Featured.jsx
@@ -47,7 +47,7 @@ function Featured() {
                 <p className="mb-0 text-lg font-semibold text-basemb-3 text-gray-700 dark:text-gray-400 ">{campaign.organisation.orgName}</p>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-lg">{campaign.description.slice(0,80)}...</p>
                </div> 
-               <button onClick={()=> handleCampaign(campaign.id)} class="inline-flex items-center mt-0 mb-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+               <button onClick={()=> handleCampaign(campaign.id)} class="inline-flex items-center mt-0 mx-5 mb-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                       Read more
                       <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
