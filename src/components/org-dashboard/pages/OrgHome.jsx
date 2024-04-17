9512bf99b67f5bbe7d60a628686686bdaae1634f
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-17 22:19:07 +0300

    Added breadcrumb to dashboard

--- src/components/org-dashboard/pages/OrgHome.jsx
+++ src/components/org-dashboard/pages/OrgHome.jsx
@@ -6,6 +6,14 @@ import RecentDonations from '../dash-components/RecentDonations'
 function OrgHome({allCampaigns,allDonations, allDonors}) {
   return (
     <div className='sm:h-fit'>
+      <div className="text-md breadcrumbs ml-2">
+          <ul>
+              <li><a href='/org/dashboard'>Home</a></li>
+              <li><a href='/org/dashboard/donations'>Donations</a></li>
+          </ul>
+      </div>
+      <h1 className="mb-3 my-2 text-2xl font-bold leading-tight ">Dashboard</h1>
+      <hr className='mb-0' />
       <QuickLinks/>
       <HomeCards allCampaigns={allCampaigns} allDonations= {allDonations}/>
       <RecentDonations allDonations= {allDonations} allDonors={allDonors} allCampaigns={allCampaigns} />
