cc41ae6ab478ef68c21e7cb6583e29a0e9baa14d
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-17 17:53:18 +0300

    Fetched usaer data and passed as props to dashboard home component

--- src/components/org-dashboard/OrgLayout.jsx
+++ src/components/org-dashboard/OrgLayout.jsx
@@ -15,11 +15,12 @@ import Donations from './pages/Donations';
 function OrgLayout() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(window.innerWidth >= 768);
   const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 768);
-  const { user, isLoggedIn } = useAuth();
   const[loading,setLoading]=useState(true)
   const[campaigns,setCampaigns]=useState([])
+  const[allDonations,setAllDonations]=useState()
   const[errors,setErrors]=useState()
   const token=localStorage.getItem('token');
+  const[donors,setDonors]=useState([])
 
   // Toggle sidebar
   const toggleSidebar = () => {
@@ -42,11 +43,67 @@ function OrgLayout() {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+//Get all donations to a logged in organisation
+  useEffect(() => {
+      const getDonations = async () => {
+          try {
+              const response = await fetch('/api/v1.0/org_donations', {
+                  method: 'GET',
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': `Bearer ${token}`
+                  },
+              });
+              const data = await response.json();
+              if (response.ok) {
+                  setLoading(true);
+                  // console.log("Successful request to get donors");
+                  setAllDonations(data);
+                  setLoading(false);
+              } else {
+                  throw new Error(data);
+              }
+          }
+          catch {
+              setErrors("Error getting donation data");
+          }
+      }
+      getDonations();
+  }, [token]);
+
+//Get donors to this organisation
+  useEffect(() => {
+    const getDonors = async () => {
+        try {
+            const response = await fetch('/api/v1.0/users', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+            });
+            const data = await response.json();
+            if (response.ok) {
+                setLoading(false);
+                console.log("Successful request to get campaigns");
+                setDonors(data);
+            } else {
+                setLoading(true);
+                throw new Error(data);
+            }
+        }
+        catch {
+            setErrors("Error getting donation data");
+        }
+    }
+    getDonors();
+
+  }, [allDonations, token]);
+
+  // console.log(donors)
 
   useEffect(() => {
       handleFetch();
-      const intervalId = setInterval(handleFetch, 7000); // Poll every 5 seconds
-      return () => clearInterval(intervalId);
   }, [token]);
   
   const handleFetch = async () => {
@@ -61,7 +118,7 @@ function OrgLayout() {
         const data = await response.json();
         if (response.ok) {
             setLoading(false)
-            console.log("Successful request to get campaigns");
+            // console.log("Successful request to get campaigns");
             setCampaigns(data.campaigns);
         } else {
             setLoading(true)
@@ -71,7 +128,7 @@ function OrgLayout() {
         setLoading(true)
         setErrors('Error in fetching campaigns', error);
     }
-};
+  };
 
 
   if  (!token){
@@ -81,15 +138,15 @@ function OrgLayout() {
   return (
     <div>
       <DashboardNav toggleSidebar={toggleSidebar} />
-      <div className="flex dark:bg-gray-900">
+      <div className="flex">
         <Menubar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
         {/* {isLargeScreen && <Menubar isOpen={isSidebarOpen} />} */}
         <main className="mt-3 mx-auto w-5/6 overflow-y-auto md:m-3 min-h-max sm:h-fit sm:w-screen lg:h-auto justify-center">
           <Routes>
-            <Route path="/" element={<OrgHome />} />
+            <Route path="/" element={<OrgHome allCampaigns={campaigns} allDonations={allDonations} allDonors={donors}/>} />
             <Route path="/createcampaign" element={<CreateCampaign handleFetching={handleFetch}/>} />
             <Route path="/campaigns" element={<CampaignCard allCampaigns={campaigns} campaignError={errors}/>} />
-            <Route path="/donations" element={<Donations allCampaigns={campaigns} handleFetching={handleFetch} campaignError={errors}/>} />
+            <Route path="/donations" element={<Donations allCampaigns={campaigns} handleFetching={handleFetch} campaignError={errors} allDonation={allDonations} allDonors={donors}/>} />
             <Route path="/accounts" element={<Accounts/>} />
             <Route path="/transaction" element={<Transaction allCampaigns={campaigns} handleFetching={handleFetch} campaignError={errors}/>} />
             <Route path="/profile" element={<Profile />} />
