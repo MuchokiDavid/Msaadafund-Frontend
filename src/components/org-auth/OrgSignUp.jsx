0edbab331b672f5696af422be1c203d26b9bbb47
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-18 18:02:27 +0300

    Added styles and removed dark mode

--- src/components/org-auth/OrgSignUp.jsx
+++ src/components/org-auth/OrgSignUp.jsx
@@ -76,81 +76,81 @@ function OrgSignUp() {
   return (
     <div>
       <Menus/>
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section className="bg-gray-50 h-screen flex justify-center items-center">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          {/* <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          {/* <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
                 <img className="w-18 h-12 mr-2" src ={logo} alt="logo"/> 
             </a> */}
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0  ">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                 Create an Organisation account
               </h1>
               {errors && <p className='text-red-500'>{errors}</p>}
               <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
                 <div>
-                  {/* <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label> */}
+                  {/* <label for="username" className="block mb-2 text-sm font-medium text-gray-900 ">Username</label> */}
                   <input type="name"
                     name="name"
                     id="company_name"
                     onChange={(e) => setUserName(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="Organisation Name"
                     required="" />
                 </div>
                 <div>
-                  {/* <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label> */}
+                  {/* <label for="username" className="block mb-2 text-sm font-medium text-gray-900 ">Username</label> */}
                   <input type="phone"
                     name="phone"
                     id="org_phone"
                     onChange={(e) => setPhoneNumber(e.target.value)}
                     maxLength={10}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="Phone Number e.g 07xxxx"
                     required="" />
                 </div>
                 <div>
-                  {/* <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label> */}
+                  {/* <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label> */}
                   <input type="email"
                     name="email"
                     id="org_email"
                     onChange={(e) => setEmail(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="E-mail e.g name@company.com"
                     required="" />
                 </div>
                 <div>
-                  {/* <label for="username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label> */}
+                  {/* <label for="username" className="block mb-2 text-sm font-medium text-gray-900 ">Username</label> */}
                   <input type="address"
                     name="address"
                     id="org_address"
                     onChange={(e) => setAddress(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="Address"
                     required="" />
                 </div>
                 <div>
-                  {/* <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label> */}
+                  {/* <label for="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label> */}
                   <input type="password"
                     name="password" id="org_password"
                     placeholder="Password"
                     onChange={(e) => setUserPassword(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     required="" />
                 </div>
                 <div>
-                  {/* <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label> */}
+                  {/* <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 ">Confirm password</label> */}
                   <input type="password"
                     name="confirm_password"
                     id="org_confirm_password"
                     placeholder="Confirm Password"
                     onChange={(e) => setConfirmPassword(e.target.value)}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     required="" />
                 </div>
-                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Already have an account? <a href="/org/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
+                <p className="text-sm font-light text-gray-500 ">
+                  Already have an account? <a href="/org/login" className="font-medium text-primary-600 hover:underline ">Login here</a>
                 </p>
               </form>
             </div>
