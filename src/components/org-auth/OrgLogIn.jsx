f4d3937f84cf5a6d6d7a81eb460bbd994a0f7496
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-18 18:03:44 +0300

    Changed login placeholder

--- src/components/org-auth/OrgLogIn.jsx
+++ src/components/org-auth/OrgLogIn.jsx
@@ -54,7 +54,7 @@ function OrgLogIn() {
                             <input type="password" 
                             name="password" 
                             id="password" 
-                            placeholder="••••••••" 
+                            placeholder="password" 
                             onChange={(e)=>{setPassword(e.target.value)}}
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" 
                             required/>
