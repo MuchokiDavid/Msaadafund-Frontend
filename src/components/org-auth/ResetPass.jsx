d83ab3de68161babdf9ba25ae638fb706d9787e1
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-18 18:08:54 +0300

    Added styling and removed dark mode

--- src/components/org-auth/ResetPass.jsx
+++ src/components/org-auth/ResetPass.jsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import axios from 'axios';
 import logo from '../../assets/msaadaBlacklogo.png';
 import toast, { Toaster } from 'react-hot-toast'
+import Menus from '../reusables/Menus';
+import Footer from '../reusables/Footer';
 
 function ResetPass() {
 const [email, setEmail] =useState('');
@@ -67,12 +69,13 @@ const handleNextStep = () => {
 
   return (
     <div>
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <Menus/>
+      <section className="bg-gray-50 h-screen flex justify-center items-center">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
             <img className="w-18 h-12 mr-2" src={logo} alt="logo" />
           </a>
-          <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
+          <div className="w-full p-6 bg-white rounded-lg shadow md:mt-0 sm:max-w-md sm:p-8">
             <div className="flex justify-between mb-4">
               <button onClick={handlePrevStep}>
                 {step === 1 ? 'Back to Login' : 'Previous'}
@@ -83,34 +86,35 @@ const handleNextStep = () => {
                 </button>
               )}
             </div>
-            <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               {step === 1 ? 'Enter Email' : 'Enter OTP and New Password'}
             </h2>
             <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" onSubmit={(e) => e.preventDefault()}>
               {step === 1 ? (
                 <div>
-                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Organisation Email</label>
-                  <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Organisation Email</label>
+                  <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" />
                 </div>
               ) : (
                 <div>
-                  <label htmlFor="otp" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">OTP</label>
-                  <input type="text" name="otp" id="otp" value={otp} onChange={(e) => setOrgOtp(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter OTP" />
-                  <label htmlFor="new-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
-                  <input type="password" name="new-password" id="new-password" value={newOrgPassword} onChange={(e) => setNewOrgPassword(e.target.value)} pattern={passwordPattern} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required="" />
-                  <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-                  <input type="password" name="confirm-password" id="confirm-password" value={confirmOrgPassword} onChange={(e) => setConfirmOrgPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required="" />
+                  <label htmlFor="otp" className="block mb-2 text-sm font-medium text-gray-900 ">OTP</label>
+                  <input type="text" name="otp" id="otp" value={otp} onChange={(e) => setOrgOtp(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Enter OTP" />
+                  <label htmlFor="new-password" className="block mb-2 text-sm font-medium text-gray-900 ">New Password</label>
+                  <input type="password" name="new-password" id="new-password" value={newOrgPassword} onChange={(e) => setNewOrgPassword(e.target.value)} pattern={passwordPattern} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="••••••••" required="" />
+                  <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 ">Confirm Password</label>
+                  <input type="password" name="confirm-password" id="confirm-password" value={confirmOrgPassword} onChange={(e) => setConfirmOrgPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="••••••••" required="" />
                 </div>
               )}
-              <button type="button" onClick={step === 1 ? handleSendOrgOTP : handleResetOrgPassword} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+              <button type="button" onClick={step === 1 ? handleSendOrgOTP : handleResetOrgPassword} className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                 {step === 1 ? 'Send OTP' : 'Reset Password'}
               </button>
             </form>
-            <p className="mt-2 text-sm text-center text-gray-500 dark:text-gray-300">{message}</p>
+            <p className="mt-2 text-sm text-center text-gray-500 ">{message}</p>
           </div>
         </div>
         <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
       </section>
+      <Footer/>
     </div>
   );
 }
