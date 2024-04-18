3638f89831999532662522839e10d0b6e3020c97
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-18 18:17:00 +0300

    Removed button to featured campaigns

--- src/components/campaigns/Featured.jsx
+++ src/components/campaigns/Featured.jsx
@@ -37,22 +37,22 @@ function Featured() {
     <h1 className="text-3xl font-bold my-4 text-center">Featured Campaigns</h1>
     <div className='my-4 mx-3 justify-center'>
         
-        <div className="mx-4 grid grid-cols-3 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4 sm:max-w-full">
+        <div className="mx-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4 sm:max-w-full">
         {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"> */}
           {featuredCampaign && featuredCampaign.map((campaign) => (
-            <div key={campaign.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+            <div key={campaign.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow" onClick={()=> handleCampaign(campaign.id)}>
               <img className="w-full rounded-t-lg h-52" src={campaign.banner} alt={campaign.campaignName} />
               <div className="px-6 py-4 flex-grow">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{campaign.campaignName}</h5>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">{campaign.campaignName.slice(0,15)}...</h5>
                 <p className="mb-0 text-lg font-semibold text-basemb-3 text-gray-700 ">{campaign.organisation.orgName}</p>
-                <p className="mb-3 font-normal text-gray-700  text-lg">{campaign.description.slice(0,80)}...</p>
+                <p className="mb-3 font-normal text-gray-700 lg:text-lg overflow-hidden">{campaign.description.slice(0,20)}...</p>
                </div> 
-               <button onClick={()=> handleCampaign(campaign.id)} class="inline-flex items-center mt-0 mx-5 mb-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+               {/* <button onClick={()=> handleCampaign(campaign.id)} class="inline-flex items-center mt-0 mx-5 mb-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                       Read more
                       <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                       </svg>
-                  </button>
+                  </button> */}
             </div>
           ))}
         </div>
