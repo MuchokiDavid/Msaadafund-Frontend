698eeee21792039ad647bcc52cac2e777500f6b5
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-19 11:58:17 +0300

    Added realtime update of the campaigns

--- src/components/campaigns/ActiveCampaigns.jsx
+++ src/components/campaigns/ActiveCampaigns.jsx
@@ -36,6 +36,9 @@ function ActiveCampaigns() {
     
       useEffect(() => {
         fetchCampaigns();
+        //use polling  for real time updates
+        const intervalId = setInterval(fetchCampaigns, 10000);
+        return () => clearInterval(intervalId);
       }, [currentPage, selectedCategory, fetchCampaigns]); // Include fetchCampaigns in the dependency array
     
       const mergeCategories = (categories) => {
@@ -76,7 +79,7 @@ function ActiveCampaigns() {
       const handleCampaign = (campaignId) => {
         setTimeout(() => {
           navigate(`/campaign/${campaignId}`);
-        }, 2000);
+        }, 1000);
       };
       //Active campaigns
       const activeCampaigns = campaigns.filter(campaign => {
@@ -121,7 +124,7 @@ function ActiveCampaigns() {
               <option className='text-lg' key={category} value={category}>{category}</option>
             ))}
           </select>
-          <div class="relative flex" data-twe-input-wrapper-init data-twe-input-group-ref>
+          <div className="relative flex" data-twe-input-wrapper-init data-twe-input-group-ref>
             <label className="input input-bordered flex items-center gap-2  dark:bg-gray-900 dark:border dark:border-gray-400">
               <input type="text" className="grow" onChange={handleSearchChange} placeholder="Search active campaign..." />
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
@@ -135,31 +138,31 @@ function ActiveCampaigns() {
         {activeCampaigns.length===0 ?
         <div className="text-xl mx-4">No Active campaigns</div>
         :
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+        <div className="mx-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4 sm:max-w-full">
           {filterCampaigns().map((campaign) => {
             return (
-            <div key={campaign.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div key={campaign.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow overflow-hidden hover:cursor-pointer" onClick={()=> handleCampaign(campaign.id)}>
               <img className="w-full rounded-t-lg h-52" src={campaign.banner} alt={campaign.campaignName} />
-              <div className="px-6 py-4 flex-grow">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{campaign.campaignName}</h5>
-                <a href=''><p className="mb-0 text-lg font-semibold text-basemb-3 text-gray-700 dark:text-gray-400 ">{campaign.organisation.orgName}</p></a>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-lg">{campaign.description.slice(0,80)}...</p>
+              <div className="py-4 flex-grow px-2">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{campaign.campaignName.slice(0,20)}</h5>
+                <a href=''><p className="mb-0 text-lg font-semibold text-basemb-3 text-blue-800 hover:underline">{campaign.organisation.orgName}</p></a>
+                <p className="mb-3 font-normal text-gray-700 text-lg">{campaign.description.slice(0,25)}...</p>
                 
-                 <button onClick={()=> handleCampaign(campaign.id)} class="inline-flex items-center mt-0 mb-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                 {/* <button onClick={()=> handleCampaign(campaign.id)} class="inline-flex items-center mt-0 mb-3 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
                     Donate now
                     <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                     </svg>
-                </button>
+                </button> */}
                
                 <div className='grid grid-flow-col grid-col-2 divide-x divide-slate-500'>
                     <div className=' px-2'>
-                        <p className="text-black text-base dark:text-white">{calculateDaysLeft(campaign.endDate) }</p>
+                        <p className="text-black text-base">{calculateDaysLeft(campaign.endDate) }</p>
                         <h6 className='text-md'>Days left</h6>
                     </div>
                     <div className='px-2'>
                     <h6 className='text-md ml-2'>Budget(Ksh)</h6>
-                        <p className="text-black ml-2 text-base dark:text-white">{campaign.targetAmount}</p>
+                        <p className="text-black ml-2 text-base">{campaign.targetAmount}</p>
                     </div>
                 </div>
                 
