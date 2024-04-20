704c2c4437710492888dba392126db3dc54e96e0
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-20 18:04:40 +0300

    Adjusted height

--- src/components/org-auth/OrgSignUp.jsx
+++ src/components/org-auth/OrgSignUp.jsx
@@ -76,7 +76,7 @@ function OrgSignUp() {
   return (
     <div>
       <Menus/>
-      <section className="bg-gray-50 h-screen flex justify-center items-center">
+      <section className="bg-gray-50 h-screen flex justify-center items-center lg:my-16">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           {/* <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
                 <img className="w-18 h-12 mr-2" src ={logo} alt="logo"/> 
