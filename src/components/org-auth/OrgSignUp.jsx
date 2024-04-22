50a1eed68dc0cf91ec91085a6bf5a862d35526b8
Clement Macharia <clementmacharia62@gmail.com>
2024-04-22 13:11:43 +0300

    implemented eye for password

--- src/components/org-auth/OrgSignUp.jsx
+++ src/components/org-auth/OrgSignUp.jsx
@@ -3,6 +3,9 @@ import Menus from '../reusables/Menus'
 import Footer from '../reusables/Footer'
 import toast, { Toaster } from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
+import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+
 
 function OrgSignUp() {
   const [message, setMessage] = useState("")
@@ -16,6 +19,8 @@ function OrgSignUp() {
   const passwordPattern = /^(?=.*[a-zA-Z!@#$%^&*])(?=.*\d)[A-Za-z\d!@#$%^&*]{8}$/
   const phonePattern = /^(07|01)\d{8}$/;
   const navigate= useNavigate()
+  const [showPassword,setShowPassword]=useState(false)
+  const [password,setPasswordConfirm ] = useState(false)
 
   const clearState = () => {
     setUserName("")
@@ -73,6 +78,16 @@ function OrgSignUp() {
     };
   }
 
+  const togglePassword=(e)=>{
+    e.preventDefault()
+    setShowPassword(!showPassword)
+  }
+
+  const togglePasswordConfirm=(e)=>{
+    e.preventDefault()
+    setPasswordConfirm(!password)
+  }
+
   return (
     <div>
       <Menus/>
@@ -96,7 +111,7 @@ function OrgSignUp() {
                     onChange={(e) => setUserName(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="Organisation Name"
-                    required="" />
+                    required/>
                 </div>
                 <div>
                   {/* <label for="username" className="block mb-2 text-sm font-medium text-gray-900 ">Username</label> */}
@@ -107,7 +122,7 @@ function OrgSignUp() {
                     maxLength={10}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="Phone Number e.g 07xxxx"
-                    required="" />
+                    required/>
                 </div>
                 <div>
                   {/* <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label> */}
@@ -117,7 +132,7 @@ function OrgSignUp() {
                     onChange={(e) => setEmail(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="E-mail e.g name@company.com"
-                    required="" />
+                    required/>
                 </div>
                 <div>
                   {/* <label for="username" className="block mb-2 text-sm font-medium text-gray-900 ">Username</label> */}
@@ -127,26 +142,26 @@ function OrgSignUp() {
                     onChange={(e) => setAddress(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                     placeholder="Address"
-                    required="" />
+                    required/>
                 </div>
-                <div>
-                  {/* <label for="password" className="block mb-2 text-sm font-medium text-gray-900 ">Password</label> */}
-                  <input type="password"
+                <div className='relative'>
+                  <input type={showPassword ? "text":"password"}
                     name="password" id="org_password"
                     placeholder="Password"
                     onChange={(e) => setUserPassword(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                    required="" />
+                    required/>
+                    <button title='show password' onClick={togglePassword} className="absolute inset-y-0 right-2 flex items-center">{showPassword?<FaEye/>:<FaEyeSlash/>}</button>
                 </div>
-                <div>
-                  {/* <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 ">Confirm password</label> */}
-                  <input type="password"
+                <div className='relative'>
+                  <input type={password ? "text" : "password"}
                     name="confirm_password"
                     id="org_confirm_password"
                     placeholder="Confirm Password"
                     onChange={(e) => setConfirmPassword(e.target.value)}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
-                    required="" />
+                    required/>
+                    <button title='show password' onClick={togglePasswordConfirm} className="absolute inset-y-0 right-2 flex items-center">{password?<FaEye/>:<FaEyeSlash/>}</button>
                 </div>
                 <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
                 <p className="text-sm font-light text-gray-500 ">
