22a60ea78bcb7a8be7779d47c8eda13868539e19
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-26 23:08:40 +0300

    added styles to donations

--- src/components/org-dashboard/pages/Donations.jsx
+++ src/components/org-dashboard/pages/Donations.jsx
@@ -164,16 +164,16 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
                             </div>
                         </div>
                         
-                        <table className="min-w-full table-zebra">
+                        <table className="min-w-full table-zebra text-xs">
                             {/* head */}
                             <thead>
                                 <tr>
-                                <th className='px-6 py-3 text-sm font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>ID</th>
-                                    <th className='px-6 py-3 text-sm font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Campaign</th>
-                                    <th className='px-6 py-3 text-sm font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Donor</th>
-                                    <th className='px-6 py-3 text-sm font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Amount</th>
-                                    <th className='px-6 py-3 text-sm font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Donation Date</th>   
-                                    <th className='px-6 py-3 text-sm font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Status</th>                           
+                                <th className='px-6 py-2 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>ID</th>
+                                    <th className='px-6 py-2 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Campaign</th>
+                                    <th className='px-6 py-2 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Donor</th>
+                                    <th className='px-6 py-2 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Amount</th>
+                                    <th className='px-6 py-2 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Donation Date</th>   
+                                    <th className='px-6 py-2 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Status</th>                           
                                 </tr>
                             </thead>
                             <tbody>
