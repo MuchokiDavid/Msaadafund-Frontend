1f2110a458a98322dc40741717ecedf746d95a07
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-26 22:56:56 +0300

    Styled donation view

--- src/components/org-dashboard/pages/Donations.jsx
+++ src/components/org-dashboard/pages/Donations.jsx
@@ -135,31 +135,35 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
 
 
     return (
-        <div className='sm:h-screen lg:h-fit mx-3'>
+        <div className='min-h-screen mx-3'>
             <div className="text-md breadcrumbs ml-2">
                 <ul>
                     <li><a href='/org/dashboard'>Dashboard</a></li>
                     <li><a href='/org/dashboard/donations'>Donations</a></li>
                 </ul>
             </div>
-            <h1 className="mb-3 my-2 text-2xl font-bold leading-tight ">My Donations</h1>
-            <hr className='mb-0' />
+            <h1 className="mb-1 my-2 text-2xl font-bold leading-tight ">My Donations</h1>
+            <hr/>
             {errors && <p className='text-red-700'>{errors}</p>}
            
             <div className='flex flex-col mt-1'>
                 <div className='py-2 -my-2 overflow-x-auto sm:-mx-2 sm:px-6 lg:-mx-2 lg:px-6'>
-                    <div className="my-5 inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
-                    <div className='flex justify-end'>
-                        <button title='Download Pdf ' onClick={downloadDonationsPDF}>PDF<FaFilePdf size = {34} style={{ color: 'red' }}/></button>
-                    </div>
-                        <input
-                            type="text"
-                            placeholder="Search by name or campaign"
-                            value={searchTerm}
-                            onChange={(e) => setSearchTerm(e.target.value)}
-                            className="px-3 py-2 border-gray-300 rounded-md mb-4 bg-gray-50 border h-11 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-1/3 p-2.5"
-                        />
-                       
+                    <div className="my-3 inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 sm:rounded-lg">
+                        <div className='flex justify-between mb-2'>
+                            <div>
+                                <input
+                                    type="text"
+                                    placeholder="Search by name or campaign"
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                    className="border-gray-300 rounded-md bg-gray-50 border h-11 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                />
+                            </div>
+                            <div className='justify-end'>
+                                <button title='Download Pdf ' onClick={downloadDonationsPDF}>PDF<FaFilePdf size = {25} style={{ color: 'red' }}/></button>
+                            </div>
+                        </div>
+                        
                         <table className="min-w-full table-zebra">
                             {/* head */}
                             <thead>
