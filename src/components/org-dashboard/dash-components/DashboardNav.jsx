cf389210f009bcc74584cc75eae5a1a8631aedab
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-26 18:01:19 +0300

    Adddd dropdown to logout user and navigate to profile

--- src/components/org-dashboard/dash-components/DashboardNav.jsx
+++ src/components/org-dashboard/dash-components/DashboardNav.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react'
-import { useAuth } from '../../../context/usersContext';
 import { MdOutlineAccountCircle } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
 import logo from  '../../../assets/msaadaLogo.png';
+import { useAuth } from '../../../context/usersContext';
 
 
 function DashboardNav({toggleSidebar}) {
     // const [isOpen, setIsOpen] = useState(true); // Default to open on large screens
   let current_org= localStorage.getItem('org');
   let token=localStorage.getItem("token");
+  const {logout} = useAuth();
 
   
   if(!token){
       window.location.replace("/org/login")
   }
 
+  const handleLogout = () => {
+    logout()
+    window.location.replace("/org/login")
+  };
+
   return (
     <div className='w-screen'>
-        <nav className="bg-slate-700 text-white flex justify-between items-center py-4 w-full">
+        <nav className="bg-slate-700 text-white flex justify-between items-center py-2 w-full">
           <div className="flex items-center ml-3">
             <button onClick={toggleSidebar} className="text-white focus:outline-none font-bold">
                 <IoMenu className="w-8 h-8"/>
@@ -28,10 +34,20 @@ function DashboardNav({toggleSidebar}) {
               
           </div>
           <div className="block ml-36 w-32 overflow-hidden">
-              <h2 className="flex items-center">
-                  <MdOutlineAccountCircle className="h-8 w-8 mr-2"/>
-                  <p className='text-lg sm:text-sm overflow-hidden'>{current_org && current_org}</p>
-              </h2>
+              <div className="flex items-center">
+                    <div className="dropdown dropdown-end absolute right-4 sm:z-20 hover:border">
+                        <div tabIndex={0} role="button" className="text-xs">
+                            <div className='flex items-center h-10'>
+                              <MdOutlineAccountCircle className="h-6 w-6 mr-2"/>
+                              <p className='text-wrap '>{current_org && current_org}</p>
+                            </div>
+                        </div>
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-slate-800 rounded-box w-36">
+                        <li className='hover:bg-slate-700'><a href="/org/dashboard/profile">Profile</a></li>
+                        <li className='hover:bg-slate-700' onClick={logout}><a href='/org/login'>Log out</a></li>
+                        </ul>
+                    </div>
+              </div>
           </div>
         </nav>
     </div>
