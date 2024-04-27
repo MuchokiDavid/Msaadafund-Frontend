db69166ad3ef0d475fba404f41eb9427e590f6cb
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-04-27 15:52:23 +0300

    Withdrawals fetched

--- src/components/org-dashboard/pages/Withdrawals.jsx
+++ src/components/org-dashboard/pages/Withdrawals.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import { all } from 'axios'
+import React, { useState, useEffect } from 'react'
 
 function Withdrawals() {
+    const[allWithdrawals, setAllWithdrawals]=useState([])
+    const token=localStorage.getItem('token')
+    const[errors,setErrors]= useState(null)
+
+    useEffect(()=>{
+        fetch('/api/v1.0/withdraw_transactions', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.message){
+               setAllWithdrawals(data.message)
+            }
+            if(data.error){
+                setErrors(data.error)
+            }
+        })
+    },[token, ])
+    console.log(allWithdrawals)
+
   return (
     <div>Withdrawals</div>
   )
