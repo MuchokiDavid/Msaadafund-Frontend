8122591891ad45fddd817c0fc95bf275667f98b1
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-05-01 16:25:01 +0300

    Added conditional rendering of donations and styles

--- src/components/org-dashboard/pages/Donations.jsx
+++ src/components/org-dashboard/pages/Donations.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 import { FaFilePdf } from "react-icons/fa";
 
-function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
+function Donations({ allCampaigns, campaignError, allDonors }) {
     const [allDonations, setAllDonations] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [errors, setErrors] = useState();
     const [campaigns, setCampaigns] = useState();
     const [donors, setDonors] = useState(allDonors);
@@ -14,6 +14,7 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
     const [filteredDonations, setFilteredDonations] = useState([]);
 
     const token = localStorage.getItem('token');
+    const orgName= localStorage.getItem('org');
 
     // Calculate total pages based on the number of items and items per page
     const totalPages = Math.ceil(filteredDonations.length / itemsPerPage);
@@ -33,6 +34,7 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
 
     useEffect(() => {
         const getDonations = async () => {
+            // setLoading(true)
             try {
                 const response = await fetch('/api/v1.0/org_donations', {
                     method: 'GET',
@@ -43,16 +45,23 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
                 });
                 const data = await response.json();
                 if (response.ok) {
-                    setLoading(false);
-                    console.log("Successful request to get donors");
-                    setAllDonations(data);
+                    if(data.message){
+                       console.log("Successful request to get donors");
+                        setAllDonations(data.message);
+                        setLoading(false); 
+                    }
+                    if(data.error){
+                        setLoading(false);
+                        setErrors(data.error);
+                    }
+                    
                 } else {
-                    setLoading(true);
-                    throw new Error(data);
+                    // setLoading(true);
+                    // throw new Error(data);
                 }
             }
             catch {
-                setErrors("Error getting donation data");
+                // setErrors("No donations found")
             }
         }
         getDonations();
@@ -62,13 +71,9 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
         setCampaigns(allCampaigns);
     }, [allCampaigns]);
 
-    useEffect(() => {
-        setErrors(campaignError)
-    }, [token, campaignError])
-
 
     useEffect(() => {
-        const filtered = allDonations.filter((donation) => {
+        const filtered = allDonations && allDonations.filter((donation) => {
             const user = donors.find(user => user.id === donation.userId);
             const donorName = user ? `${user.firstName} ${user.lastName}` : "";
             const campaign = campaigns.find(campaign => campaign.id === donation.campaignId);
@@ -78,8 +83,8 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
         setFilteredDonations(filtered);
     }, [allDonations, searchTerm, donors, campaigns]);
 
-    if (!token) {
-        window.location = '/org/login';
+    if (!token && !orgName){
+        window.location.href = '/org/login';
     }
     if (loading) {
         return(<div className='flex justify-center'><span className="loading loading-dots loading-lg"></span></div>)
@@ -135,7 +140,7 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
 
 
     return (
-        <div className='min-h-screen mx-3'>
+        <div>
             <div className="text-sm breadcrumbs ml-2">
                 <ul>
                     <li><a href='/org/dashboard'>Dashboard</a></li>
@@ -145,75 +150,86 @@ function Donations({ allCampaigns, campaignError, allDonation, allDonors }) {
             <h1 className="mb-1 my-2 text-2xl font-bold leading-tight ">My Donations</h1>
             <hr/>
             {errors && <p className='text-red-700'>{errors}</p>}
-           
-            <div className='flex flex-col mt-1'>
-                    <div className='flex justify-between py-1'>
-                        <div>
-                            <input
-                                type="text"
-                                placeholder="Search by name or campaign"
-                                value={searchTerm}
-                                onChange={(e) => setSearchTerm(e.target.value)}
-                                className="border-gray-300 rounded-md bg-gray-50 border h-11 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                            />
+           {allDonations && allDonations.length>0
+                ?
+                (
+                <div>
+                    <div className='flex flex-col mt-1'>
+                        <div className='flex justify-between py-1'>
+                            <div>
+                                <input
+                                    type="text"
+                                    placeholder="Search by name or campaign"
+                                    value={searchTerm}
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                    className="border-gray-300 rounded-md bg-gray-50 border h-11 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                />
+                            </div>
+                            <div>
+                                <button title='Download Pdf ' onClick={downloadDonationsPDF}>PDF<FaFilePdf size = {25} style={{ color: 'red' }}/></button>
+                            </div>
                         </div>
-                        <div>
-                            <button title='Download Pdf ' onClick={downloadDonationsPDF}>PDF<FaFilePdf size = {25} style={{ color: 'red' }}/></button>
+                        <div className="my-1 inline-block min-w-full overflow-scroll align-middle border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full table-zebra text-xs overflow-x-auto">
+                                {/* head */}
+                                <thead>
+                                    <tr>
+                                    <   th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>ID</th>
+                                        <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Campaign</th>
+                                        <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Donor</th>
+                                        <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Amount</th>
+                                        <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Donation Date</th>   
+                                        <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Status</th>                           
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {paginatedDonations.map((donation) => {
+                                        const user = donors.find(user => user.id === donation.userId);
+                                        const donorName = user ? `${user.firstName} ${user.lastName}` : "Anonymous";
+                                        const campaign = campaigns.find(campaign => campaign.id === donation.campaignId);
+                                        const campaignTitle = campaign ? campaign.campaignName : "";
+                                        return (
+                                            <tr key={donation._id}>
+                                                <td className='px-4 py-1 whitespace-no-wrap border-b border-gray-200 '>{donation.id}</td>
+                                                <td className='px-4 py-1 whitespace-no-wrap border-b border-gray-200 '>{campaignTitle}</td>
+                                                <td className='px-6 py-1 whitespace-no-wrap border-b border-gray-200 '>{donorName}</td>
+                                                <td className='px-6 py-1 whitespace-no-wrap border-b border-gray-200'>{donation.amount}</td>
+                                                <td className='px-6 py-1 whitespace-no-wrap border-b border-gray-200 '>{moment(donation.donationDate).format('dddd Do MMMM, YYYY')}</td>
+                                                <td className='px-6 py-1 whitespace-no-wrap border-b border-gray-200'>{donation.status}</td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                    <div className="my-1 inline-block min-w-full overflow-scroll align-middle border-b border-gray-200 sm:rounded-lg">
-                        <table className="min-w-full table-zebra text-xs overflow-x-auto">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                <   th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>ID</th>
-                                    <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Campaign</th>
-                                    <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Donor</th>
-                                    <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Amount</th>
-                                    <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Donation Date</th>   
-                                    <th className='px-6 py-1 font-medium leading-4 tracking-wider text-left text-white uppercase border-b border-gray-200 bg-emerald-400'>Status</th>                           
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {paginatedDonations.map((donation) => {
-                                    const user = donors.find(user => user.id === donation.userId);
-                                    const donorName = user ? `${user.firstName} ${user.lastName}` : "Anonymous";
-                                    const campaign = campaigns.find(campaign => campaign.id === donation.campaignId);
-                                    const campaignTitle = campaign ? campaign.campaignName : "";
-                                    return (
-                                        <tr key={donation._id}>
-                                            <td className='px-4 py-1 whitespace-no-wrap border-b border-gray-200 '>{donation.id}</td>
-                                            <td className='px-4 py-1 whitespace-no-wrap border-b border-gray-200 '>{campaignTitle}</td>
-                                            <td className='px-6 py-1 whitespace-no-wrap border-b border-gray-200 '>{donorName}</td>
-                                            <td className='px-6 py-1 whitespace-no-wrap border-b border-gray-200'>{donation.amount}</td>
-                                            <td className='px-6 py-1 whitespace-no-wrap border-b border-gray-200 '>{moment(donation.donationDate).format('dddd Do MMMM, YYYY')}</td>
-                                            <td className='px-6 py-1 whitespace-no-wrap border-b border-gray-200'>{donation.status}</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-            </div>
-            <div className="flex justify-center mb-4 join grid-cols-2">
-                {/* Previous page button */}
-                <button className="btn btn-outline join-item" onClick={() => goToPage(currentPage - 1)} disabled={currentPage === 1}>
-                    Previous
-                </button>
+                    <div className="flex justify-center mb-4 join grid-cols-2">
+                        {/* Previous page button */}
+                        <button className="btn btn-outline join-item" onClick={() => goToPage(currentPage - 1)} disabled={currentPage === 1}>
+                            Previous
+                        </button>
 
-                {/* <div className='border border-gray-400 flex justify-center p-2 btn-outline w-fit'>{currentPage} of {totalPages}</div> */}
-                {/* Next page button */}
-                <button
-                    className="btn btn-outline join-item"
-                    onClick={() => goToPage(currentPage + 1)}
-                    disabled={currentPage === totalPages}
-                >
-                    Next
-                </button>
-            </div>
+                        {/* <div className='border border-gray-400 flex justify-center p-2 btn-outline w-fit'>{currentPage} of {totalPages}</div> */}
+                        {/* Next page button */}
+                        <button
+                            className="btn btn-outline join-item"
+                            onClick={() => goToPage(currentPage + 1)}
+                            disabled={currentPage === totalPages}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </div>
+            )
+            :
+            (
+                <div className='flex justify-center'>
+                    <p className='text-xl'>No donations yet</p>
+                </div>
+            )
+           }
         </div>
     )
 }
 
 export default Donations;
-
