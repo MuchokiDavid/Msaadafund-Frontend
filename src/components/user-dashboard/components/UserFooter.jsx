fb601bd6c1991316d59353b48cdb81b741208db3
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-05-05 22:23:46 +0300

    Added user footer

---
+++ src/components/user-dashboard/components/UserFooter.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Copyright from '../../reusables/Copyright'
+
+function Footer() {
+  return (
+    <div>
+        <footer className="bg-sky-950 text-white py-2 text-sm shadow mt-4">
+            <div className="ml-4">
+                <div className="row">
+                <div className="col-md-4 mb-4">
+                    <h5 className='text-xl mb-1'>About Us</h5>
+                    <p>Empowering you to make contributions smarter, faster, and more securely.</p>
+                    <p>Reach out to us through <a href='mailto:msaadacontact@gmail.com' className='text-md text-blue-300'>Msaada Team</a> for any questions or feedback.</p>
+                </div>
+                </div>
+            </div>
+        </footer>
+        <Copyright/>
+    </div>
+  )
+}
+
+export default Footer
