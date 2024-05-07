7aee54ffd4898371e7e6527a34e99dafc0b94b14
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-05-08 01:58:12 +0300

    added dashboard cards to show statistics to dashboard

--- src/components/user-dashboard/pages/UserHome.jsx
+++ src/components/user-dashboard/pages/UserHome.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import DashboardBanner from '../components/DashboardBanner'
 import RecentDonation from '../components/RecentDonation'
+import DashCards from '../components/DashCards'
 
-function UserHome({allDonations}) {
+function UserHome({allDonations, allSubscriptions}) {
   return (
     <div>
       <DashboardBanner/>
+      <DashCards allDonations= {allDonations} allSubscriptions= {allSubscriptions}/>
       <RecentDonation allDonations= {allDonations}/>
     </div>
   )
