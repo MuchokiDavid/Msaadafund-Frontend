7983f47776dedd7ab6854f8caedb84d3c9b205ec
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-05-07 18:30:39 +0300

    sliced user

--- src/components/user-dashboard/components/UserNav.jsx
+++ src/components/user-dashboard/components/UserNav.jsx
@@ -36,7 +36,7 @@ function UserNav({toggleSidebar}) {
                     <div className="dropdown dropdown-end absolute right-4 sm:z-20">
                         <div tabIndex={0} role="button" className="text-xs">
                             <div className='flex items-center h-10 bg-blue-600 text-white border border-blue-600 hover:bg-transparent focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-xs px-1 py-2.5 text-center'>
-                              <p className='text-wrap '>{current_user && current_user}</p>
+                              <p className='text-wrap '>{current_user && current_user.slice(0,5)}...</p>
                               <MdOutlineAccountCircle className="h-6 w-6 ml-2"/>
                             </div>
                         </div>
