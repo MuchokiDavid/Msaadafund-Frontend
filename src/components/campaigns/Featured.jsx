3ca59de59037258ab2ce083818a3ac79ded06aa9
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-05-11 14:18:21 +0300

    Added styles and layout

--- src/components/campaigns/Featured.jsx
+++ src/components/campaigns/Featured.jsx
@@ -86,7 +86,7 @@ function Featured() {
                             </div>
                             <div>
                               <dt className="sr-only">Organiser</dt>
-                              <dd><a href='#' className='text-blue-700 hover:underline whitespace-nowrap'>{campaign.organisation.orgName}</a></dd>
+                              <dd><a href='#' className='text-blue-700 hover:underline whitespace-nowrap'>Organiser: {campaign.organisation.orgName}</a></dd>
                               {/* <a href='#' className='text-blue-700 hover:underline text-base overflow-hidden'><dd>{campaign.organisation.orgName}</dd></a> */}
                             </div>
                           </dl>
