d01d0b45d082ec42420379b7d431104053a4948e
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-05-15 03:16:03 +0300

    Active and past campaigns created

--- src/components/campaigns/OrgActive.jsx
+++ src/components/campaigns/OrgActive.jsx
@@ -23,7 +23,7 @@ function OrgActive({organisationDetails}) {
 
   return (
     <div>
-        <h1 className='text-xl'>Active campaigns</h1>
+        <h1 className='text-xl mb-4'>Active campaigns</h1>
         {/* -------------------------------------Cards for campaign--------------------------------------- */}
         {activeCampaigns.length === 0 ? <div className="text-xl mx-4 min-h-screen">No Active campaigns</div> : null}
         <div className="mx-2 sm:mx-1 lg:mx-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 sm:gap-2 md:gap-4 sm:max-w-full">
@@ -32,13 +32,13 @@ function OrgActive({organisationDetails}) {
                 return(
                 <Link to = {`/campaign/${campaign.id}`} key={campaign.id}>
                 <div className="card w-auto bg-base-100 rounded-md shadow-lg">
-                <figure className='h-40'><img src={campaign.banner} alt={campaign.campaignName} loading='lazy'/></figure>
+                <figure><img src={campaign.banner} alt={campaign.campaignName} className='h-56 w-full rounded-t-md object-cover' loading='lazy'/></figure>
                 <div className="card-body">
-                  <h2 className="card-title">
+                  <h2 className="card-title font-medium overflow-hidden text-lg whitespace-nowrap hover:text-blue-600 hover:cursor-pointer">
                   {campaign.campaignName}
                     {/* <div className="badge badge-secondary">NEW</div> */}
                   </h2>
-                  <p>{campaign.description.slice(0,35)}...</p>
+                  <p>{campaign.description.slice(0,25)}...</p>
                   <div className="card-actions justify-end">
                     <div className="badge badge-outline">{campaign.category}</div> 
                     <div className="badge badge-outline">{calculateDaysLeft(campaign.endDate)} Days</div>
