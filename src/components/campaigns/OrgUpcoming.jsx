7cdac97ae6ccfe9cc622a8092d3061bac4f8c085
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-05-18 05:26:51 +0300

    organisation profile designed and completed

---
+++ src/components/campaigns/OrgUpcoming.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
+function OrgUpcoming({organisationDetails}) {
+    const currentDate = new Date();
+    //Upcoming campaigns
+    const upcomingCampaigns = organisationDetails.filter(campaign => {
+        const startDate = new Date(campaign.startDate);
+        return startDate > currentDate;
+    });
+
+    // const handleAlert=()=>{
+    //     Swal.fire({
+    //         icon: 'error',
+    //         title: 'Oops...',
+    //         text: 'Campaign is already closed!',
+    //       })
+    // }
+
+    return (
+        <div className='py-4'>
+        {/* <h1 className='text-xl my-4'>Past campaigns</h1> */}
+        {/* -------------------------------------Cards for campaign--------------------------------------- */}
+        {upcomingCampaigns.length === 0 ? <div className="text-xl mx-4 mb-6">No Upcoming campaigns to display</div> : null}
+        <div className="mx-2 sm:mx-1 lg:mx-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 sm:gap-2 md:gap-4 sm:max-w-full">
+            {/* <div className="mx-2 sm:mx-1 lg:mx-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 sm:gap-2 md:gap-4 sm:max-w-full"> */}
+            {upcomingCampaigns && upcomingCampaigns.map((campaign)=>{
+                    return(
+                    <Link to = {`/campaign/${campaign.id}`} key={campaign.id}>
+                    <div className="card w-auto bg-base-100 rounded-md shadow-lg key={campaign.id}">
+                        <figure className='h-40'><img src={campaign.banner} alt={campaign.campaignName} loading='lazy'/></figure>
+                        <div className="card-body">
+                        <h2 className="card-title">
+                        {campaign.campaignName}
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>{campaign.description.slice(0,30)}...</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">{campaign.category}</div> 
+                        </div>
+                        </div>
+                    </div>
+                </Link>
+                )
+            })}                
+        </div>
+
+    </div>
+    )
+    }
+
+export default OrgUpcoming
---
+++ src/pages/PublicOrgProfile.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { IoMdNotifications } from "react-icons/io";
+
+
+function Profile({orgName,orgType, subscribe,handleSubscribe,handleUnsubscribe, profileImage}) {
+  return (
+    <div className='bg-white rounded-lg pb-3 border'>
+  <div className="h-24 overflow-hidden" id='orgBanner'>
+    {/* <img className="object-cover object-top w-full" src="https://source.unsplash.com/random/1920x1080/?organisation-profile" alt='Mountain'/> */}
+  </div>
+  <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+    <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+      <div className="flex">
+        <img className="h-24 w-24 rounded-full ring-2 ring-blue-300 sm:h-32 sm:w-32" src={profileImage} alt="Org profile"/>
+      </div>
+
+      <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+        <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
+          <h1 className="truncate text-2xl font-bold text-blue-300">{orgName}</h1>
+          <p className="mt-1 text-sm text-gray-500">{orgType}</p>
+        </div>
+        <div className="mt-6 flex flex-col justify-stretch space-y-3 sm:flex-row sm:space-x-4 sm:space-y-0">
+          {subscribe ? (
+            <button type="button" className="inline-flex justify-center rounded-md border border-blue-600 bg-blue-600 px-3 py-2 text-sm font-semibold text-white hover:text-gray-800 shadow-md hover:bg-gray-50" onClick={handleUnsubscribe}>
+             <IoMdNotifications className="text-lg mr-2" /> Unsubscribe
+            </button>
+          ) : (
+            <button type="button" className="inline-flex justify-center rounded-md border border-blue-600 bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm  hover:text-gray-800 hover:bg-gray-50" onClick={handleSubscribe}>
+              <IoMdNotifications className="text-lg mr-2" />Subscribe
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+    <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
+      <h1 className="truncate text-2xl font-bold text-blue-300">{orgName}</h1>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default Profile
