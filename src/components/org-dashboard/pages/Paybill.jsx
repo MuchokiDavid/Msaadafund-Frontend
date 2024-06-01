7bd5ae0c31163c27686ba61cb4986b296cadfd38
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-06-01 21:39:09 +0300

    connected with the backend

--- src/components/org-dashboard/pages/Paybill.jsx
+++ src/components/org-dashboard/pages/Paybill.jsx
@@ -8,7 +8,7 @@ function Paybill({allCampaigns,campaignError,handleWallet}) {
     const [campaign, setCampaign] = useState('')
     const [walletDetails, setWalletDetails] = useState('')
     const [error, setError] = useState('')
-    const [transactionResponse, setTransactionResponse] = useState('')
+    const [transactionResponse, setTransactionResponse] = useState([])
     const [paybillNumber, setPaybillNumber]= useState('')
     const [accountNumber, setAccountNumber]= useState('')
     const [amount, setAmount]= useState(10)
@@ -89,15 +89,16 @@ const handlePay = async (e) => {
             Swal.fire({
                 icon: 'success',
                 title: 'Request successful',
-                text: 'Your payment request has been received successfully'
+                text: response.data.message
             });
         }
         
     } catch (error) {
         console.error('Error in making payment:', error);
-        setError('Error in making payment');
+        setError(error.response.data.error);
         setLoading(false);
         setIsSubmitting(false);
+        setTransactionResponse([])
     }
 };
 
@@ -140,30 +141,6 @@ const handleSubmit = (e) => {
     }
 };
 
-if (loading) {
-    return (
-        <div aria-label="Loading..." role="status" className="flex justify-center items-center space-x-2  min-h-screen">
-            <svg className="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
-                <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
-                <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round" stroke-linejoin="round"
-                    stroke-width="24"></line>
-                <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                </line>
-                <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
-                    stroke-width="24"></line>
-                <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                </line>
-                <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
-                    stroke-width="24"></line>
-                <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
-                <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                </line>
-            </svg>
-        <span className="text-4xl font-medium text-gray-500">Loading...</span>
-    </div>
-    )
-}
-
 // console.log(transactionResponse)
 
   return (
@@ -176,11 +153,10 @@ if (loading) {
         </div>
         <div>
             <h1 className="font-extrabold text-2xl">Paybill</h1>
-            <hr className='mb-2'/>
-            {error && <p className='text-red-600 text-base my-2'>{error}</p>}
+            <hr className='mb-2'/>            
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                 <form ref={formRef} onSubmit={handleSubmit}>                    
-                    {transactionResponse.transactions && <p className='text-emerald-500'>Status: {transactionResponse.transactions[0].status}</p>}
+                    {/* {transactionResponse && transactionResponse.transactions && <p className='text-emerald-500'>Status: {transactionResponse.transactions[0].status}</p>} */}
                     {walletDetails? 
                         <div className="stats border">
                             <div className="stat">
@@ -190,6 +166,7 @@ if (loading) {
                         </div>
                         : null
                     }
+                    {error && <p className='text-red-600 text-base my-2'>{error}</p>}
                     <div>
                         <label className="block font-semibold" htmlFor="name"><span className='text-red-500'>*</span>Campaign</label>
                         <select 
