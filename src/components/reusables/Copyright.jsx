9adc249117dbfab935a257dad931bf4862022a20
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-06-13 17:28:44 +0300

    added featured to campaign details

--- src/components/reusables/Copyright.jsx
+++ src/components/reusables/Copyright.jsx
@@ -12,8 +12,9 @@ function Copyright() {
     }, [])
     
   return (
-    <div className='flex justify-between bg-sky-950'>
-        <div className='text-white text-xs text-left bg-sky-950 h-8 p-2'>
+    <div className='flex justify-center bg-sky-950'>
+      <hr className='bg-slate-50'/>
+        <div className='text-white text-xs text-center bg-sky-950 h-8 p-2'>
         © {currentYear} Msaada-Mashinani. All rights reserved.
       </div>
       <div>
