c823a8cdfd7d0f74fd4078478599c59fcec91deb
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-06-15 15:12:52 +0300

    added logo to loading

--- src/components/campaigns/Featured.jsx
+++ src/components/campaigns/Featured.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { prettyNumber } from '@based/pretty-number'
+import logo from '../../assets/msaadaLogo.png'
 
 function Featured() {
     const [featuredCampaign, setFeaturedCampaign] = useState(null)
@@ -81,7 +82,7 @@ function Featured() {
               <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
               </line>
           </svg>
-          <span className="text-4xl font-medium text-gray-500">Loading...</span>
+          <span className="text-4xl font-medium text-gray-500"><img src={logo} alt='Loading...' className='w-1/2 h-1/2'/></span>
       </div>
       )
     }
