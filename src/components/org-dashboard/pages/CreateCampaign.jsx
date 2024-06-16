3f78cddc1efaa6a20c6bcc44e0770b031354f285
Clement Macharia <clementmacharia62@gmail.com>
2024-06-16 22:26:51 +0300

    dark mode edit

--- src/components/org-dashboard/pages/CreateCampaign.jsx
+++ src/components/org-dashboard/pages/CreateCampaign.jsx
@@ -140,7 +140,7 @@ function CreateCampaign({getValidYoutubeVideoId}) {
                     </div>
                     <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-4 mb-4">
                         <div>
-                            <label htmlFor="startDate" className="block mb-2 text-sm font-semibold text-slate-600 ">
+                            <label htmlFor="startDate" className="block mb-2 text-sm font-semibold text-slate-600  ">
                             <span className='text-red-500'>*</span>Start Date
                             </label>
                             <input
@@ -148,7 +148,7 @@ function CreateCampaign({getValidYoutubeVideoId}) {
                                 type="date"
                                 value={startDate}
                                 onChange={(e) => setStartDate(e.target.value)}
-                                className="bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                className="bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5"
                                 required
                             />
                         </div>
