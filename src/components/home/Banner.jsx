8b019467f836ba62a659be3b13074616547cc0ea
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-06-28 12:30:06 +0300

    Changed loading style

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -4,7 +4,6 @@ import { prettyNumber } from '@based/pretty-number'
 import BannerSlider from './BannerSlider'
 import BannerCards from './BannerCards'
 import joinus from '../../assets/joinus.jpg'
-import logo from '../../assets/msaadaLogo.png'
 
 function Banner() {
   const[allDonations,setAllDonations]= useState([])
@@ -110,25 +109,13 @@ function Banner() {
   if(loading){
     // return(<div className='flex justify-center'><span className="loading loading-dots loading-lg"></span></div>)
     return (
-      <div aria-label="Loading..." role="status" className="flex justify-center items-center space-x-2  min-h-screen bg-transparent">
-        <svg className="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
-            <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
-            <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="24"></line>
-            <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-            </line>
-            <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="24"></line>
-            <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-            </line>
-            <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
-                stroke-width="24"></line>
-            <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
-            <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-            </line>
-        </svg>
-        <span className="text-4xl font-medium text-gray-500"><img src={logo} alt='Loading...' className='w-1/2 h-1/2'/></span>
-    </div>
+      <div class="flex items-center justify-center h-screen">
+        <div class="relative">
+            <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+            <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
+            </div>
+        </div>
+      </div>
     )
   }
   
--- src/pages/CampainDetails.jsx
+++ src/pages/CampainDetails.jsx
@@ -228,25 +228,13 @@ function CampainDetails() {
 
     if (!campaign) {
         return (
-            <div aria-label="Loading..." role="status" className="flex justify-center items-center space-x-2  min-h-screen">
-                <svg className="h-20 w-20 animate-spin stroke-gray-500" viewBox="0 0 256 256">
-                    <line x1="128" y1="32" x2="128" y2="64" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
-                    <line x1="195.9" y1="60.1" x2="173.3" y2="82.7" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="24"></line>
-                    <line x1="224" y1="128" x2="192" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                    </line>
-                    <line x1="195.9" y1="195.9" x2="173.3" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="24"></line>
-                    <line x1="128" y1="224" x2="128" y2="192" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                    </line>
-                    <line x1="60.1" y1="195.9" x2="82.7" y2="173.3" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="24"></line>
-                    <line x1="32" y1="128" x2="64" y2="128" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line>
-                    <line x1="60.1" y1="60.1" x2="82.7" y2="82.7" stroke-linecap="round" stroke-linejoin="round" stroke-width="24">
-                    </line>
-                </svg>
-            <p className="text-4xl font-medium text-gray-500"><img src={logo} alt='Loading...' className='w-1/2 h-1/2'/></p>
-        </div>
+            <div class="flex items-center justify-center h-screen">
+                <div class="relative">
+                    <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+                    <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
+                    </div>
+                </div>
+            </div>
         )
     }
 
