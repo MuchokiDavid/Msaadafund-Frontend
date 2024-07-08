8b3ff65601ad3221b083e504cf5d53f050be0c0f
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-07-09 02:12:39 +0300

    added react tour provider

--- src/index.js
+++ src/index.js
@@ -5,17 +5,77 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/usersContext';
 import { BrowserRouter } from 'react-router-dom';
+import { TourProvider } from '@reactour/tour'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const steps = [
+  {
+    selector: '.dash',
+    content: 'Welcome to MsaadaFund! This is your dashboard, where you can easily navigate through all the features of the app.',
+  },
+  {
+    selector: '.create-campaign',
+    content: 'To start a new fundraising effort, click here to create a new campaign.',
+  },
+  {
+    selector: '.view-campaigns',
+    content: 'Here, you can view all the campaigns you have created and manage them.',
+  },
+  {
+    selector: '.view-contributions',
+    content: 'Check all contributions made to your campaigns by visiting this section.',
+  },
+  {
+    selector: '.view-signatories',
+    content: 'Add and manage your signatories, who will approve transactions, by clicking here.',
+  },
+  {
+    selector: '.add-withdrawal-account',
+    content: 'Set up your M-Pesa and Bank withdrawal accounts in this section to enable fund withdrawals.',
+  },
+  {
+    selector: '.withdraw-funds',
+    content: 'To transfer your campaign funds to your withdrawal accounts, click here.',
+  },
+  {
+    selector: '.paybills',
+    content: 'You can pay your bills directly from your campaign funds by visiting this section.',
+  },
+  {
+    selector: '.buy-goods',
+    content: 'Purchase goods and services using your campaign funds by clicking here.',
+  },
+  {
+    selector: '.airtime',
+    content: 'Buy airtime for your phone using your campaign funds from this section.',
+  },
+  {
+    selector: '.view-withdrawals',
+    content: 'View the history of all your withdrawals here.',
+  },
+  {
+    selector: '.view-transactions',
+    content: 'Check all transactions related to your campaigns in this section.',
+  },
+  {
+    selector: '.view-approvals',
+    content: 'See all pending approvals from your signatories for various actions related to your campaigns here.',
+  },
+  {
+    selector: '.view-quick',
+    content: 'Quickly access important features of the app here.',
+  },
+];
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <TourProvider steps={steps}>
     <AuthProvider>
       <App />
     </AuthProvider>
-    
-    </BrowserRouter>
-    
+    </TourProvider>
+    </BrowserRouter>    
   </React.StrictMode>
 );
 
