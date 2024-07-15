54ce1f2125c1d004edf3ae2124cebb30e4aaaa59
Clement Macharia <clementmacharia62@gmail.com>
2024-07-16 00:39:29 +0300

    loading in banner

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -106,18 +106,18 @@ function Banner() {
     setButtonClicked(false)
   }, [buttonClicked]);
 
-  if(loading){
-    // return(<div className='flex justify-center'><span className="loading loading-dots loading-lg"></span></div>)
-    return (
-      <div class="flex items-center justify-center h-screen">
-        <div class="relative">
-            <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-            <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
-            </div>
-        </div>
-      </div>
-    )
-  }
+  // if(loading){
+  //   // return(<div className='flex justify-center'><span className="loading loading-dots loading-lg"></span></div>)
+  //   return (
+  //     <div class="flex items-center justify-center h-screen">
+  //       <div class="relative">
+  //           <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+  //           <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
+  //           </div>
+  //       </div>
+  //     </div>
+  //   )
+  // }
   
   function getTotalAmount(donationsArray) {
     let totalAmount = 0;
