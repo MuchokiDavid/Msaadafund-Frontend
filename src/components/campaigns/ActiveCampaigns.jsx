3171b773e511d87a72e8ac6d2bc2dd6b88d15645
Clement Macharia <clementmacharia62@gmail.com>
2024-07-15 12:56:00 +0300

    backend route

--- src/components/campaigns/ActiveCampaigns.jsx
+++ src/components/campaigns/ActiveCampaigns.jsx
@@ -17,7 +17,7 @@ function ActiveCampaigns() {
     const fetchCampaigns = useCallback(async () => {
         try {
           setLoading(true)
-          const response = await fetch(`/api/v1.0/campaigns?page=${currentPage}&category=${selectedCategory}`);
+          const response = await fetch(`https://appbackend.msaadafund.com/api/v1.0/campaigns?page=${currentPage}&category=${selectedCategory}`);
           if (!response.ok) {
             throw new Error('Failed to fetch campaigns');
           }
