d8f741c0298c7fb4daab4c1f277d92c990605840
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-07-19 20:45:37 +0300

    Login in or register button

--- src/components/user-auth/Register.jsx
+++ src/components/user-auth/Register.jsx
@@ -193,7 +193,7 @@ function Register() {
                 <button type="submit" disabled={!policy} className={`w-full text-white ${policy ? 'bg-primary-600':'bg-gray-600'} focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center`}>Create an account</button>
                 <Googleauth/>
                 <p className="text-sm font-light text-gray-500">
-                  Already have an account? <a href="/user/login" className="font-medium text-primary-600 hover:underline ">Login here</a>
+                  Already have an account? <a href="/user/login" className="btn btn-sm font-medium text-primary-600 hover:underline ">Login here</a>
                 </p>
               </form>
               </div>
