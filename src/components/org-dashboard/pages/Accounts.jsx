3bdde2c75602c46430181d6db753b72be00aaf66
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-07-21 00:09:37 +0300

    Added loading button

--- src/components/org-dashboard/pages/Accounts.jsx
+++ src/components/org-dashboard/pages/Accounts.jsx
@@ -206,10 +206,10 @@ function Accounts({banks, fetchBank}) {
                 </button>
                 {error && <p className='text-red-500 mt-4'>{error}</p>}
                 <div className="accounts-list-section w-full">
-                    <h1 className='text-2xl font-normal mb-4 text-slate-600 '>Your Accounts</h1>
+                    <h1 className='text-xl font-normal mb-4 text-slate-600 '>Your Accounts</h1>
                     <div className='overflow-scroll'>
                         <table className='w-full border-collapse border border-gray-300 text-left'>
-                            <thead className='text-xl font-normal' >
+                            <thead className='text-left text-base'>
                                 <tr className='bg-gray-100'>
                                     <th className='border border-gray-300 px-2 py-1 text-gray-700 '>Providers</th>
                                     <th className='border border-gray-300 px-2 py-1 text-gray-700 '>Bank</th>                                  
@@ -258,18 +258,18 @@ function Accounts({banks, fetchBank}) {
                 )}
                 {showCreateAccount && (
                     // Create Account Popup JSX
-                    <div className="create-account-popup fixed top-0 left-0 w-full min-h-screen overflow-y-auto flex items-center justify-center bg-gray-900 bg-opacity-50 z-50 py-4">
-                        <div className='mx-auto lg:max-w-md md:max-w-full sm:max-w-full p-6 bg-white rounded-lg shadow-md text-white h-fit'>
-                            <div className='flex justify-between'>
+                    <div className="create-account-popup fixed top-0 left-0 w-full min-h-screen overflow-auto flex items-center justify-center bg-gray-900 bg-opacity-50 z-50 py-4">
+                        <div className="mx-auto lg:max-w-md md:max-w-full sm:max-w-full p-6 bg-white overflow-y-auto rounded-lg shadow-md text-white max-h-screen">
+                            <div className="flex justify-between">
                                 <div>
-                                    <h1 className='text-2xl font-semibold mb-4 text-slate-600 '>Create Account</h1>
+                                    <h1 className="text-2xl font-semibold mb-4 text-slate-600">Create Account</h1>
                                 </div>
                                 <div>
-                                    <button onClick={handleClosePopup}  className='hover:border hover:bg-gray-100 rounded-full text-gray-800 text-2xl w-10 h-10 flex justify-center items-center'><IoClose /></button>
+                                    <button onClick={handleClosePopup} className="hover:border hover:bg-gray-100 rounded-full text-gray-800 text-2xl w-10 h-10 flex justify-center items-center"><IoClose /></button>
                                 </div>
                             </div>
                             
-                            <div className='mb-6 flex items-center justify-center'>
+                            <div className="mb-6 flex items-center justify-center">
                                 <form onSubmit={handleSubmit} className='w-full' ref={formRef}>
                                 {error && <p className='text-red-500 mt-4'>{error}</p>}
                                     <div className='mb-4'>
@@ -323,9 +323,26 @@ function Accounts({banks, fetchBank}) {
                                         <input type='password' value={confirmPin} onChange={(e) => setConfirmPin(e.target.value)} className='block text-gray-700 w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-primary-600' placeholder='confirm pin' maxLength={4} required />
                                     </div>
                                     <div>
-                                        <button type='submit' className='bg-blue-600 hover:bg-blue-700 text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
+                                        {loading ?
+                                            (
+                                                <button type="button" class="btn btn-md py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg max-w-md">
+                                                    <svg width="20" height="20" fill="currentColor" class="mr-2 animate-spin" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                                                        <path d="M526 1394q0 53-37.5 90.5t-90.5 37.5q-52 0-90-38t-38-90q0-53 37.5-90.5t90.5-37.5 90.5 37.5 37.5 90.5zm498 206q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-704-704q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm1202 498q0 52-38 90t-90 38q-53 0-90.5-37.5t-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-964-996q0 66-47 113t-113 47-113-47-47-113 47-113 113-47 113 47 47 113zm1170 498q0 53-37.5 90.5t-90.5 37.5-90.5-37.5-37.5-90.5 37.5-90.5 90.5-37.5 90.5 37.5 37.5 90.5zm-640-704q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm530 206q0 93-66 158.5t-158 65.5q-93 0-158.5-65.5t-65.5-158.5q0-92 65.5-158t158.5-66q92 0 158 66t66 158z">
+                                                        </path>
+                                                    </svg>
+                                                    Saving
+                                                </button>
+                                            ) 
+                                            :
+                                            (
+                                                <button type="submit" class="btn btn-md py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg max-w-md">
+                                                    Save
+                                                </button>
+                                            )
+                                        }
+                                        {/* <button type='submit' className='bg-blue-600 hover:bg-blue-700 text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
                                             {loading ? 'Saving...' : 'Save'}
-                                        </button>
+                                        </button> */}
                                     </div>
                                 </form>
                             </div>
