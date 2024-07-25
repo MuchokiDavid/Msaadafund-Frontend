0867d2bbeeb7a5007a0b84e6a8b1e391501bdd58
Clement Macharia <clementmacharia62@gmail.com>
2024-07-25 16:25:15 +0300

    babel console.log removed

---
+++ config-overrides.js
@@ -0,0 +1,5 @@
+const { override, addBabelPlugin } = require('customize-cra');
+
+module.exports = override(
+  process.env.NODE_ENV === 'production' && addBabelPlugin('transform-remove-console')
+);
