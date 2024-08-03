34d02f71e461a22dff9a9bc4f7c621c7f5979843
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-08-04 01:24:33 +0300

    Changed loading style

--- src/components/campaigns/ActiveCampaigns.jsx
+++ src/components/campaigns/ActiveCampaigns.jsx
@@ -97,11 +97,7 @@ function ActiveCampaigns() {
         // return(<div className='flex justify-center'><span className="loading loading-dots loading-lg"></span></div>)
         return (
           <div class="flex items-center justify-center h-screen">
-              <div class="relative">
-                  <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-                    <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
-                    </div>
-              </div>
+              <span className="loading loading-dots loading-lg text-blue-500"></span>
             </div>
           )
       }
--- src/components/org-auth/OrgLogIn.jsx
+++ src/components/org-auth/OrgLogIn.jsx
@@ -15,32 +15,18 @@ function OrgLogIn() {
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate()
-  const [loading, setLoading]= useState(false)
   
 
   const login = (e) =>{
     e.preventDefault();
     orgLogin(email, password);
   }
-  
-  if (loading){
-    return (
-      <div class="flex items-center justify-center h-screen">
-        <div class="relative">
-          <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
-            <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin">
-             </div>
-          </div>
-      </div>
-    )
-  }
+
 
   if (isLoggedIn) {
-    setLoading(true)
     setTimeout(() => {
       navigate('/org/dashboard')
-      setLoading(false)
-    }, 1000);
+    }, 2000);
     return null;
   }
   // if (isLoggedIn) {
