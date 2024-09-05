6036d9c787e36e5aa7ed247490648323c360c37d
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-09-05 20:38:04 +0300

    Added google translator key and url

--- src/context/Utils.js
+++ src/context/Utils.js
@@ -1,3 +1,5 @@
 export const appKey = "a394ae0e-e763-41b5-8964-78d8fefab274"
 export const apiUrl= "https://backend.service.msaadafund.com/home"
+export const apiKey = 'AIzaSyCphvvum_UuoDPpeflUZMbV3b_cVxKoZnY';
+export const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
 // export const apiUrl= "http://127.0.0.1:5555"
