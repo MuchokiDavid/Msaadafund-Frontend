c5e80b6a7ddfff782347add76690b15b9b18acf2
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-10-23 19:14:15 +0300

    google recapture packages

--- src/context/Utils.js
+++ src/context/Utils.js
@@ -1,5 +1,7 @@
 export const appKey = "a394ae0e-e763-41b5-8964-78d8fefab274"
 export const apiUrl= "https://backend.service.msaadafund.com/home"
 export const apiKey = 'AIzaSyCphvvum_UuoDPpeflUZMbV3b_cVxKoZnY';
+export const recaptureSecret= '6Lf27WkqAAAAAN65LufQPQPIERkkv4i8LoFYgjSv'
+export const recaptchaKey= '6Lf27WkqAAAAAP5JKhtqgzvsJX4ajzskUJreJaRZ'
 export const url = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
 // export const apiUrl= "http://127.0.0.1:5555"
